5d45b123d3128b5080769e6c5ec030d841558ee2
Anaïs Zafiriou-Lejeune <anais.ljn@outlook.fr>
2024-03-07 10:17:36 +0100

    Ajout de redux redux toolkit + creation composant Formulaire

--- src/ComponentsBase/Nav/Nav.js
+++ src/ComponentsBase/Nav/Nav.js
@@ -8,7 +8,7 @@ function Nav() {
     return (
         <nav className="main-nav">
             <NavLink className="main-nav-logo" to="/">
-                <img className="main-nav-logo-image" src="./img/argentBankLogo.png" alt="Argent Bank Logo" />
+                <img className="main-nav-logo-image" src="./img/argentBankLogo.webp" alt="Argent Bank Logo" />
                 <h1 className="sr-only">Argent Bank</h1>
             </NavLink>
             <div>
