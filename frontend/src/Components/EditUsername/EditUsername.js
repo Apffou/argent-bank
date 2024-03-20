3bf52c4f02a927bf37ec459eef4d37e91fdbcf1a
Anaïs Zafiriou-Lejeune <anais.ljn@outlook.fr>
2024-03-20 10:42:40 +0100

    MaJ du style du form Edit

--- frontend/src/Components/EditUsername/EditUsername.js
+++ frontend/src/Components/EditUsername/EditUsername.js
@@ -1,30 +1,35 @@
+import './EditUsername.scss';
 import { useDispatch, useSelector } from "react-redux"
 import { selectUserProfile } from "../../Redux/Selectors";
+
 
 export default function EditUsername() {
 
     //Fonction dispatch de Redux
     const dispatch = useDispatch();
-
+    // Utilisation du sélecteur pour extraire les propriétés du profil utilisateur
     const { firstName, lastName, userName } = useSelector(selectUserProfile);
 
     return (
         <div>
-            <div class="header">
+            <div className="header">
                 <h1>Welcome back<br />{userName} !</h1>
                 <button class="edit-button">Edit Name</button>
             </div>
-            <div>
+            <div className='form-content'>
+                <h2>Edit User Info</h2>
                 <form>
                     <label htmlFor="">User Name</label>
                     <input type="text" placeholder={userName} />
-                    <label htmlFor="">First Name {firstName} </label>
+                    <label htmlFor="">First Name </label>
                     <input type="text" placeholder={firstName} readOnly />
                     <label htmlFor="">Last Name</label>
                     <input type="text" placeholder={lastName} readOnly />
                 </form>
-                <button class="button-form-edit">Save</button>
-                <button class="button-form-edit">Cancel</button>
+                <div className='buttons-edition'>
+                    <button class="button-form-edit" type='submit'>Save</button>
+                    <button class="button-form-edit cancel" type='button'>Cancel</button>
+                </div>
             </div>
         </div>
     )
