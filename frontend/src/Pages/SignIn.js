9377a5623f84fdf2944fab3021941079a76a5338
Anaïs Zafiriou-Lejeune <anais.ljn@outlook.fr>
2024-03-28 19:22:19 +0100

    Remise en forme du css (erreur en supprimant une erreur console)

--- frontend/src/Pages/SignIn.js
+++ frontend/src/Pages/SignIn.js
@@ -4,7 +4,7 @@ import Form from "../Components/Form/Form";
 
 function SignIn() {
     return (
-        <>
+        <div className="main-contain">
             <main className="main bg-dark">
                 <section className="sign-in-content">
                     <i className="fa fa-user-circle sign-in-icon"></i>
@@ -12,7 +12,7 @@ function SignIn() {
                     <Form />
                 </section>
             </main>
-        </>
+        </div>
     )
 }
 export default SignIn;
